e444e4f35e8ad9a473749d9069b3037b39dd63ba
Kunal Rustagi <kunrusta@publicisgroupe.net>
2020-09-17 18:57:06 +0530

    Initial Commit

---
+++ FirstCoreProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCoreProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
